c5aed1cac07a331fc047c2a0024cc78840b05c22
Elliot <elliotrauchwerger@gmail.com>
2015-09-07 00:49:32 -0400

    Added main.c skeleton for us to branch from

---
+++ main.c
@@ -0,0 +1,29 @@
+/* ****************************************************************************
+** COP 4610 -- Project 1
+** Due September 21, 2015
+** Team Members:
+** Elliot Rauchwerger, Michael Anderson, Juan Llaguno
+**
+** Main.c
+******************************************************************************/
+
+
+int main()
+{
+    char *line;
+    char **cmd;
+
+    while (1)
+    {
+	/*
+	my_setup();
+	my_prompt();
+	line = my_read();
+	cmd = my_parse(line);
+	my_exectue(cmd);
+	my_clean();
+	*/
+    }
+
+    return 0;
+}
